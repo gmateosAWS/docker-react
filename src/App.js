5a6f6e58bebe98e9e9560d795555189c5c86ea3e
Gus <gmateos.aws@gmail.com>
2023-01-21 18:02:00 +0100

    Keep learn react text in home to pass app tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch!
+          Learn react text changed in new feature!
         </a>
       </header>
     </div>
